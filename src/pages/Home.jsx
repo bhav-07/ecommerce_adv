f4a082faa4005d474ca048a76728e854af1e2f6b
bhav-07 <jangirbhavani201@gmail.com>
2023-04-19 14:14:19 +0530

    Created navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Announcement from '../components/Announcement'
 
 const Home = () => {
   return (
-    <div><Navbar/></div>
+    <div>
+      <Announcement/>
+      <Navbar/>
+      </div>
   )
 }
 
