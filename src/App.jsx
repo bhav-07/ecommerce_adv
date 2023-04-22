cca7a2926d6c3fb50e057cdccf13b786f11d2e6b
bhav-07 <jangirbhavani201@gmail.com>
2023-04-22 17:48:34 +0530

    created cart page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 // import Login from "./pages/Login";
-import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+// import Login from "./pages/Login";
 // import Register from "./pages/Register";
 // import Product from "./pages/Product";
 // import Home from "./pages/Home";
 // import ProductList from "./pages/ProductList";
 
 const App=()=>{
-    return <Login/>;
+    return <Cart/>;
 }
 
 export default App;
