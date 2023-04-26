33915029fcd6753cdbca50da549b2183f856dea3
bhav-07 <jangirbhavani201@gmail.com>
2023-04-26 16:26:56 +0530

    formatted

--- api/index.js
+++ api/index.js
@@ -1,17 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose=require('mongoose');
-const dotenv=require('dotenv');
-const userRoute=require('./routes/user');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
-const connectionParams={
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-}
+const connectionParams = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
 
-mongoose.connect(process.env.MONGO_URL,connectionParams).then(()=>{console.info("Connected to database")}).catch((e)=>{console.log("Error",e);});
+mongoose
+  .connect(process.env.MONGO_URL, connectionParams)
+  .then(() => {
+    console.info("Connected to database");
+  })
+  .catch((e) => {
+    console.log("Error", e);
+  });
 
 app.use(express.json());
-app.use("/api/users",userRoute);
-app.listen(process.env.PORT || 5000,()=>console.log("Server started"));
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.listen(process.env.PORT || 5000, () => console.log("Server started"));
