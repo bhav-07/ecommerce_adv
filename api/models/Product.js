8b00450d29fe442ea81aa1c9e6f3ce5114f6fef4
bhav-07 <jangirbhavani201@gmail.com>
2023-04-27 17:36:15 +0530

    fixed a bug

--- api/models/Product.js
+++ api/models/Product.js
@@ -1,17 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-//Creating the schema to store the user data
 const ProductSchema = new mongoose.Schema(
-    {
-        title: { type: String, required: true, unique: true },
-        desc: { type: String, required: true },
-        img: { type: String, required: true },
-        categories: { type: Array, required: true },
-        size: { type: String },
-        color: { type: String },
-        price: { type: Number, required: true },
-    },
-    { timestamps: true }
+  {
+    title: { type: String, required: true, unique: true },
+    desc: { type: String, required: true },
+    img: { type: String, required: true },
+    categories: { type: Array, required: true },
+    size: { type: String },
+    color: { type: String },
+    price: { type: Number, required: true },
+  },
+  { timestamps: true }
 );
 
-module.exports = mongoose.model('User', ProductSchema);
+module.exports = mongoose.model("Product", ProductSchema);
