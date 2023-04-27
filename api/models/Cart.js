ec216f8a370e39f1edc061a0112c95f4246a9624
bhav-07 <jangirbhavani201@gmail.com>
2023-04-27 18:29:33 +0530

    Fixed typo

--- api/models/Cart.js
+++ api/models/Cart.js
@@ -1,18 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 //Creating the schema to store the user data
 const CartSchema = new mongoose.Schema(
-    {
-        userId: { type: String, required: true },
-        products:[
-            {
-                productId: { type: String, required: true },
-                quantity: { type: Number, required: true, default: 1 },
-                
-            }
-        ],
-    },
-    { timestamps: true }
+  {
+    userId: { type: String, required: true },
+    products: [
+      {
+        productId: { type: String, required: true },
+        quantity: { type: Number, required: true, default: 1 },
+      },
+    ],
+  },
+  { timestamps: true }
 );
 
-module.exports = mongoose.model('User', CartSchema);
+module.exports = mongoose.model("Cart", CartSchema);
--- api/models/Order.js
+++ api/models/Order.js
@@ -1,22 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 //Creating the schema to store the user data
 const OrderSchema = new mongoose.Schema(
-    {
-        userId: { type: String, required: true },
-        products:[
-            {
-                productId: { type: String, required: true },
-                quantity: { type: Number, required: true, default: 1 },
-                
-            }
-        ],
-        amount: { type: Number, required: true },
-        address: { type: Object, required: true },
-        status: { type: String, default: "pending" },
-
-    },
-    { timestamps: true }
+  {
+    userId: { type: String, required: true },
+    products: [
+      {
+        productId: { type: String, required: true },
+        quantity: { type: Number, required: true, default: 1 },
+      },
+    ],
+    amount: { type: Number, required: true },
+    address: { type: Object, required: true },
+    status: { type: String, default: "pending" },
+  },
+  { timestamps: true }
 );
 
-module.exports = mongoose.model('User', OrderSchema);
+module.exports = mongoose.model("Order", OrderSchema);
