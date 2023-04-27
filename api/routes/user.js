c551b4d4475d1c2746f7cdc80a1df74701b62016
bhav-07 <jangirbhavani201@gmail.com>
2023-04-27 17:34:35 +0530

    added user route and fixed a typo

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,13 +1,94 @@
-const router = require('express').Router();
+const User = require("../models/User");
+const {
+  verifyToken,
+  verifyTokenandAuth,
+  verifyTokenandAdmin,
+} = require("./verifytoken");
+const router = require("express").Router();
 
-router.get("/usertest", (req, res) => {
-    res.send("User test");
+//* UPDATE
+router.put("/:id", verifyTokenandAuth, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString();
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post("/userposttest", (req, res) => {
-    const username=req.body.username;
-    res.send(username);
-    console.log(username)
-}); 
+//*DELETE Method
+
+router.delete("/:id", verifyTokenandAuth, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted");
+  } catch (error) {
+    res.status(500).json("error");
+  }
+});
+
+//*GET USER
+
+router.get("/find/:id", verifyTokenandAdmin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc; // ... is the spread operator
+    res.status(200).json(others);
+  } catch (error) {
+    res.status(500).json("error");
+  }
+});
+
+//*GET ALL USERS
+router.get("/", verifyTokenandAdmin, async (req, res) => {
+  const query = req.query.new;
+  try {
+    const users = query
+      ? await User.find().sort({ id: -1 }).limit(5)
+      : await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json("error");
+  }
+});
+
+//* GET USER STATS
+router.get("/stats", verifyTokenandAdmin, async (req, res) => {
+  const date = new Date();
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+  try {
+    const data = await User.aggregate([
+      { $match: { createdAt: { $gte: lastYear } } },
+      {
+        $project: {
+          month: { $month: "CreatedAt" },
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
